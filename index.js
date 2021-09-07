e920a2b467a7096b9f41d4ba29cefc3e4f63adfb
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-07 11:40:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+// console.log(mutiply);
+
+const random = Math.round(Math.random()) + 1
+//=> 1 or 2
+
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+// console.log(mod)
+
+let array = [1, 2, 3, 4, 20]
+const max = Math.max(...array)
+// console.log(max)
